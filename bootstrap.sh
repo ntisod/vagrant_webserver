e3752b77220c22ebc472cf071e656b50ffeabebb
ntisodbojo <bo.johansson@sodertalje.ntig.se>
2016-09-07 14:41:43 +0200

    Update bootstrap to 14.04

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
+if ! [ -L /var/www/html ]; then
+  rm -rf /var/www/html
+  ln -fs /vagrant /var/www/html
 fi
